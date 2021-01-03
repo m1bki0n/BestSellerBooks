b0206f94c4c63e8be4fe5624aef9333b8b0990fa
Makwan BK <contact@makwanbk.com>
2021-01-03 16:15:38 +0300

    Initial Commit

---
+++ Shared/BestSellerBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestSellerBooksApp.swift
+//  Shared
+//
+//  Created by Makwan BK on 2021-01-03.
+//
+
+import SwiftUI
+
+@main
+struct BestSellerBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
